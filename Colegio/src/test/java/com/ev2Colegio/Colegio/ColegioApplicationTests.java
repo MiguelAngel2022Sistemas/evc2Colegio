f4fc18f83ce24b4e0b56e9ffb2f96c77b39062ae
Wilder Julio Espinoza Bravo <wilderespinoza@bcp.com.pe>
2023-07-07 15:11:59 -0500

    Guardar Datos Face1

---
+++ Colegio/src/test/java/com/ev2Colegio/Colegio/ColegioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev2Colegio.Colegio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColegioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
